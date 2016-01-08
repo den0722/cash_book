d96e65b9f41bb84dfd593c3dbaf0d8a21ac8807e
Fmy <fmy.feliz.mobile@gmail.com>
2016-01-08 16:50:42 +0900

    ver1_hirano

---
+++ cash_book/app/Test/Fixture/DetailFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * DetailFixture
+ *
+ */
+class DetailFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'detail_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 64, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'created' => '2016-01-08 08:12:14',
+			'modified' => '2016-01-08 08:12:14',
+			'detail_name' => 'Lorem ipsum dolor sit amet'
+		),
+	);
+
+}
---
+++ cash_book/app/Test/Fixture/JournalFixture.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * JournalFixture
+ *
+ */
+class JournalFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'payee_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'detail_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'amount' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'payed_date' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'created' => '2016-01-08 08:33:13',
+			'modified' => '2016-01-08 08:33:13',
+			'payee_id' => 1,
+			'detail_id' => 1,
+			'amount' => 1,
+			'payed_date' => '2016-01-08 08:33:13'
+		),
+	);
+
+}
